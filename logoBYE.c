323ff001666dca3004f8153f19912dad0a09613d
ganeshreddy1877 <ganeshreddydharmagari@gmail.com>
2025-07-22 12:21:22 +0530

    Add files via upload

---
+++ logoBYE.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    printf("BBBB      YY    YY     EEEEEE\n");
+    printf("BB   BB    Y    Y      EE\n");
+    printf("BB  BB      Y  Y       EE\n");
+    printf("BBBB         Y         EEEEEE\n");
+    printf("BB   BB      Y         EE\n");
+    printf("BB  BB       Y         EE\n");
+    printf("BBBB         Y         EEEEEE\n");
+    return(0);
+}
